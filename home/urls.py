14031e67497d008801217b17c0a4a9ceb979c26d
PAUL PENNUEL ECIL <paulpennuel@gmail.com>
2024-08-19 06:40:16 +0300

    working version template

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('services/<str:service_name>/', views.service, name='service'),
+
+    path('retreats/', views.retreats, name='retreats'),
+    path('retreats/<str:retreat_name>/', views.retreat, name='retreat'),
+    path ('rooms/', views.rooms, name='rooms'),
 ]
--- home/views.py
+++ home/views.py
@@ -9,3 +9,34 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def service(request):
+    return render(request, 'service-details.html')
+
+
+def retreats(request):
+    return render(request, 'retreats.html')
+
+
+def retreat(request, retreat_name):
+    if request.method == 'POST':
+        comment = request.POST.get('comment')
+        author = request.POST.get('author')
+        email = request.POST.get('email')
+        save_info = request.POST.get('save_info')
+        if request.method == 'POST':
+            comment = request.POST.get('comment')
+            author = request.POST.get('author')
+            email = request.POST.get('email')
+            save_info = request.POST.get('save_info')
+
+    return render(request, 'retreats-details.html', {'retreat_name': retreat_name})
+
+
+def rooms(request,room_name):
+    return request(request, 'rooms.html', {'room_name': room_name})
