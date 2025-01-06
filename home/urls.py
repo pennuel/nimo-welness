75225533701a06707a79ee2c685fc658719903b7
PAUL PENNUEL ECIL <56873997+pennuel@users.noreply.github.com>
2025-01-07 00:28:08 +0300

    Fixed the errors from previous commits but add the service and services details

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('service/<str:services_selected>/', views.service_details, name='service'),
-    path('services', views.service, name='service')
+    path('services/', views.service, name='service')
 ]
