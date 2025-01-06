4c6afafd705ec86388ac464b14e5dba34d763455
PAUL PENNUEL ECIL <56873997+pennuel@users.noreply.github.com>
2025-01-06 22:42:42 +0300

    added more content to the python

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('service/<str:services_selected>/', views.service_details, name='service'),
+    path('services', views.service, name='service')
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from constants import services_dict
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
@@ -9,3 +9,34 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+# Add the template from prompt engineering for the services.
+    # 'mindfulness': {
+    #     'title': 'Mindfulness',
+    #     'header text': 'Mindfulness',
+    #     'title_details': 'describes what we are doing in the service',
+    #     'description': 'give an indepth description of the service and how we do it and what i will do from the person',
+    #     'benefits': [
+    #         "benefits in a list format"
+    #     ],
+    #     'assurances': 'assurances of the service and what it can do for the customer',
+    # },
+
+
+    
+
+def service_details(request, services_selected):
+
+    service_details = services_dict.get(services_selected)
+    services_title = [title for title, _ in services_dict.items()]
+    print(services_title)
+    if service_details:
+        return render(request, 'service-details.html', {'service': service_details,'titles' : services_title})
+    else:
+        return render(request, '404.html', status=404)
+
+
+def service(request):
+    services = [(title,service) for title, service in services_dict.items()]
+    print(services)
+    return render(request, 'services.html', {'services': services})
